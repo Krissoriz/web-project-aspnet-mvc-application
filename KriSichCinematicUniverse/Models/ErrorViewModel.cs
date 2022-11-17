8295ffac5d1d3b59dea1c04b717f92291ff73bd2
user <user@LAPTOP-PM3B5UAB>
2022-11-17 22:16:17 +0200

    Add project files.

---
+++ KriSichCinematicUniverse/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KriSichCinematicUniverse.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
